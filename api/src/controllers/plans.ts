f62bdaf506eb918e04982ca5f956e8c762c41d5f
Dong Nghi Vo <nghi.vodong942003@gmail.com>
2023-10-28 22:23:15 +0300

    Initial plan model

---
+++ api/src/controllers/plans.ts
@@ -0,0 +1,8 @@
+import { RequestHandler } from 'express';
+
+import Plan from '../models/Plan';
+import User from '../models/User';
+
+export const addMeal : RequestHandler = async (req, res) => {
+
+};
---
+++ api/src/models/Plan.ts
@@ -0,0 +1,32 @@
+import { InferSchemaType, model, Schema, Types } from 'mongoose';
+
+const MealSchema = new Schema({
+  time_slot : {
+    type : String,
+    required : true
+  },
+  recipe : {
+    type : Types.ObjectId,
+    required : true,
+    ref : 'Recipe'
+  }
+});
+
+const PlanSchema = new Schema({
+  user : {
+    type : Types.ObjectId,
+    required : true,
+    ref : 'User'      
+  },
+  date : {
+    type : Date,
+    required : true,
+    expires: 60 * 60 * 24 * 7 //expires after 1 week 
+  },
+  meals : [MealSchema]
+});
+
+type IPlan = InferSchemaType<typeof PlanSchema> & { _id: Types.ObjectId };
+
+const Plan = model<IPlan>('Plan', PlanSchema);
+export default Plan;
