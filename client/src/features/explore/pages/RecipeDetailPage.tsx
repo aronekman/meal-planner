b54c9364ddbd9c12af6551615bb1ceb460894546
Aron <aron99ekman@gmail.com>
2023-11-06 18:47:24 +0200

    fix linter error

--- client/src/features/explore/pages/RecipeDetailPage.tsx
+++ client/src/features/explore/pages/RecipeDetailPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo } from 'react';
+import { useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 import { useAppContext } from '@/common/AppContext';
