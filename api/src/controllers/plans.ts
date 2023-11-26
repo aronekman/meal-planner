dab9919bc8f86f252cfb1e0dd5db077b250db0e9
Dong Nghi Vo <nghi.vodong942003@gmail.com>
2023-11-26 04:18:09 +0200

    plan controller sync problem debugged

--- api/src/controllers/plans.ts
+++ api/src/controllers/plans.ts
@@ -1,4 +1,5 @@
 import { RequestHandler } from 'express';
+import mongoose from 'mongoose';
 
 import Plan from '../models/Plan.js';
 import { verifyDatetime } from '../utils/timestring.js';
@@ -15,21 +16,25 @@ export const getPlan: RequestHandler = async (req, res) => {
   if (!verifyDatetime(queryDate)) {
     return res.status(400).json({ error: 'Invalid date' });
   }
-  const plan = await Plan.findOne({ user: req.user?._id, date: queryDate }).populate('meals.recipe');
-  if (!plan) {
-    const newPlan = await Plan.create({
-      user: req.user?._id,
-      date: queryDate,
-      meals: []
+
+  const session = await mongoose.startSession();
+  await session.withTransaction(async () => {
+    const plan = await Plan.findOne({ user: req.user?._id, date: queryDate }).populate('meals.recipe');
+    if (!plan) {
+      const newPlan = await Plan.create({
+        user: req.user?._id,
+        date: queryDate,
+        meals: []
+      });
+      return res.status(200).send(newPlan);
+    }
+    plan.meals.sort((meal1, meal2) => {
+      if (!meal1.time_slot) return 1;
+      if (!meal2.time_slot) return -1;
+      return meal1.time_slot < meal2.time_slot ? -1 : meal1.time_slot > meal2.time_slot ? 1 : 0;
     });
-    return res.status(200).send(newPlan);
-  }
-  plan.meals.sort((meal1, meal2) => {
-    if (!meal1.time_slot) return 1;
-    if (!meal2.time_slot) return -1;
-    return meal1.time_slot < meal2.time_slot ? -1 : meal1.time_slot > meal2.time_slot ? 1 : 0;
+    return res.status(200).send(plan);
   });
-  res.status(200).send(plan);
 };
 
 export const addMeal: RequestHandler = async (req, res) => {
